5f5b54c98c242734437bd1d3df45ab1c66437f8b
StivenHerrera <stiven3722@gmail.com>
2022-09-16 13:56:26 -0500

    Actualizacion final

--- main.js
+++ main.js
@@ -1,59 +1,54 @@
-function escribirUno(){
-    let resultado = document.getElementById('resultado');
-    resultado.value += 1; 
-   
+function escribirUno() {
+  let resultado = document.getElementById("resultado");
+  resultado.value += 1;
 }
 function escribirDos() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 2;
- 
 }
 function escribirTres() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 3;
-  
 }
 function escribirCuatro() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 4;
-  
 }
 function escribirCinco() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 5;
- 
 }
 function escribirSeis() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 6;
 }
 function escribirSiete() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 7;
 }
 function escribirOcho() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 8;
 }
 function escribirNueve() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 9;
 }
 function escribirCero() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value += 0;
 }
 function escribirResultado() {
-  let resultado = document.getElementById('resultado');
-  resultado.value = eval (resultado.value);
+  let resultado = document.getElementById("resultado");
+  resultado.value = eval(resultado.value);
 }
 function escribirBorrar() {
-  let resultado = document.getElementById('resultado');
+  let resultado = document.getElementById("resultado");
   resultado.value = "";
 }
-function escribirMulti(){
-    let resultado = document.getElementById('resultado');
-    resultado.value += "*";
+function escribirMulti() {
+  let resultado = document.getElementById("resultado");
+  resultado.value += "*";
 }
 function escribirDivision() {
   let resultado = document.getElementById("resultado");
@@ -69,7 +64,7 @@ function escribirResta() {
 }
 function escribirPorcentaje() {
   let resultado = document.getElementById("resultado");
-  resultado.value = 100/resultado.value;
+  resultado.value = 100 / resultado.value;
 }
 function escribirRaiz() {
   let resultado = document.getElementById("resultado");
@@ -79,19 +74,15 @@ function escribirRaizC() {
   let resultado = document.getElementById("resultado");
   resultado.value = Math.cbrt(resultado.value);
 }
-function escribirPI(){
-      let resultado = document.getElementById("resultado");
-      resultado.value = Math.PI;
-    
+function escribirPI() {
+  let resultado = document.getElementById("resultado");
+  resultado.value = Math.PI;
 }
-function escribirPotencia(){
-    let resultado = document.getElementById("resultado");
-    resultado.value = Math.pow(resultado.value);
-
+function escribirPotencia() {
+  let resultado = document.getElementById("resultado");
+  resultado.value = Math.pow(resultado.value);
 }
-
-
-
-
-
-
+function escribirPunto() {
+  let resultado = document.getElementById("resultado");
+  resultado.value += ".";
+}
